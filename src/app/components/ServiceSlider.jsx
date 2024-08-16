a3e1ece48b4544d02625f1b417e2a179b7bd2836
satyapal106 <palsatya1999@gmail.com>
2024-08-16 12:46:26 +0530

    update service images

--- src/app/components/ServiceSlider.jsx
+++ src/app/components/ServiceSlider.jsx
@@ -48,7 +48,7 @@ const ServiceSlider = () => {
               <SwiperSlide>
                 <div className="newsCard news-Slide-up">
                   <Image 
-                  src="/assets/images/slider/service1.jpg" 
+                  src="/assets/images/services/1.jpg" 
                   width={500}
                   height={250}
                   alt="Service 1" 
@@ -62,7 +62,7 @@ const ServiceSlider = () => {
               </SwiperSlide>
               <SwiperSlide>
                 <div className="newsCard news-Slide-up">
-                  <Image src="/assets/images/slider/service1.jpg"
+                  <Image src="/assets/images/services/2.jpg"
                    width={500}
                    height={250} 
                   alt="Service 2" />
@@ -76,7 +76,7 @@ const ServiceSlider = () => {
               <SwiperSlide>
                 <div className="newsCard news-Slide-up">
                   <Image 
-                    src="/assets/images/slider/service1.jpg" 
+                    src="/assets/images/services/3.jpg" 
                     width={500}
                     height={250}
                     alt="Service 3" 
@@ -91,7 +91,7 @@ const ServiceSlider = () => {
               <SwiperSlide>
                 <div className="newsCard news-Slide-up">
                   <Image 
-                  src="/assets/images/slider/service1.jpg" 
+                  src="/assets/images/services/4.jpg" 
                   width={500}
                   height={250}
                   alt="Service 4" 
